3c6674b5a341f6ee2d1f1db974d9d2e0de182e19
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-11 10:27:58 +0200

    doctor registration form #9

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Doctor;
 use App\Entity\Patient;
 use App\Entity\User;
+use App\Form\DoctorFormType;
 use App\Form\RegistrationFormType;
 use App\Service\NameFormatterService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -43,4 +45,33 @@ class RegistrationController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+	#[Route('/doctor', name: 'app_register_doctor')]
+	public function newDoctor(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+	{
+		$user = new Doctor();
+		$form = $this->createForm(DoctorFormType::class, $user);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$user->setFirstname(NameFormatterService::formatName($form->get('firstname')->getData()));
+
+			$user->setPassword(
+				$userPasswordHasher->hashPassword(
+					$user,
+					$form->get('password')->getData()
+				)
+			);
+
+			$entityManager->persist($user);
+			$entityManager->flush();
+			// do anything else you need here, like send an email
+
+			return $this->redirectToRoute('app_login');
+		}
+
+		return $this->render('registration/doctor.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
 }
