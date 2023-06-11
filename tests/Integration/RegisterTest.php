9e582a03871013d47cba632b042d2bff4ebac394
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-11 12:22:58 +0200

    update tests #9

--- tests/Integration/RegisterTest.php
+++ tests/Integration/RegisterTest.php
@@ -29,6 +29,33 @@ class RegisterTest extends WebTestCase
 
 		$client->submit($form);
 
-		$this->assertResponseRedirects('/');
+		$this->assertResponseRedirects('/login');
+	}
+
+	public function testRegisterDoctor(): void
+	{
+		$faker = Factory::create();
+
+		$client = static::createClient();
+		$crawler = $client->request('GET', '/doctor');
+
+		$this->assertResponseIsSuccessful();
+
+		$form = $crawler->selectButton('Register')->form();
+
+		$password = $faker->password();
+
+		$form['doctor_form[firstname]'] = $faker->firstName();
+		$form['doctor_form[lastname]'] = $faker->lastName();
+		$form['doctor_form[email]'] = $faker->email();
+		$form['doctor_form[phone]'] = $faker->phoneNumber();
+		$form['doctor_form[password][first]'] = $password;
+		$form['doctor_form[password][second]'] = $password;
+		$form['doctor_form[speciality]'] = '1';
+		$form['doctor_form[agreeTerms]'] = true;
+
+		$client->submit($form);
+
+		$this->assertResponseRedirects('/login');
 	}
 }
