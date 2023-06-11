432df9aade389c23bc0d138008b95f5f88c4a5b3
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-11 10:17:51 +0200

    update registration form #9

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Service\NameFormatterService;
@@ -17,7 +18,7 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        $user = new User();
+        $user = new Patient();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Patient;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -80,7 +81,7 @@ class RegistrationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Patient::class,
         ]);
     }
 }
