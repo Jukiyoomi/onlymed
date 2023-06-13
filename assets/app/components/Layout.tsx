68735fa67ba0f454dbc916438df08c6452f1887b
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-13 19:24:09 +0200

    update layout & add routes in index #15

--- assets/app/components/Layout.tsx
+++ assets/app/components/Layout.tsx
@@ -1,11 +1,12 @@
-import Header from "@comps/Header";
 import React from "react";
+import Header from "@comps/Header";
+import {BrowserRouter as Router} from "react-router-dom";
 
 export default function Layout({children}: {children: React.ReactNode}) {
 	return (
-		<>
+		<Router>
 			<Header />
 			{children}
-		</>
+		</Router>
 	);
 }
--- assets/app/index.tsx
+++ assets/app/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "@pages/Home";
+import Layout from "@comps/Layout";
 
 export default function App() {
-	  return (
-		  <div></div>
-	  );
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<h1>Not Found</h1>} />
+			</Routes>
+		</Layout>
+	);
 }
