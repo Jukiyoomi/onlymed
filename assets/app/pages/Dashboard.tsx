a2096d603fbc95e048d62970bd30487542623d88
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-15 10:51:39 +0200

    api route #15
    - create php dashboard route
    - get data in react dashboard

--- assets/app/pages/Dashboard.tsx
+++ assets/app/pages/Dashboard.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Container from "@comps/Container";
 import Button from "@comps/Button";
+import {useQuery} from "@tanstack/react-query";
 
 export default function Dashboard() {
+	const {data, isLoading} = useQuery(['user'], () =>  {
+		return fetch('/api/dashboard').then(res => res.json())
+	})
+
+	if (isLoading) return <div>Loading...</div>
+
 	return (
 		<Container className="dashboard">
 			<section className="dashboard_header">
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class DefaultController extends AbstractController
 {
@@ -13,4 +17,20 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig');
     }
+
+	#[Route('/api/dashboard', name: 'app.dashboard', methods: ['GET'])]
+	public function hello(#[CurrentUser] ?User $user, SerializerInterface $serializer): JsonResponse
+	{
+		if (!$user) {
+			return $this->json([
+				'user' => null,
+				'error' => 'User not found'
+			], Response::HTTP_NOT_FOUND);
+		}
+
+		return $this->json([
+			'user' => $user,
+			'error' => null
+		], Response::HTTP_OK);
+	}
 }
