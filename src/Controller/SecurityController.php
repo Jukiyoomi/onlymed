63dc1d01cccd18aaadd631c2fcdaa2649f4e5fda
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-15 11:04:33 +0200

    updated some redirections

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, #[CurrentUser] ?User $user): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($user) {
+             return $this->redirectToRoute('app_default');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
