8411f4273160c1b92255bddf35599e2a73410b39
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-06-19 19:20:28 +0200

    infinite scroll feature #16

--- assets/app/pages/SearchDoctor.tsx
+++ assets/app/pages/SearchDoctor.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Container from "@comps/Container";
-import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
-import RatingStars from "@comps/RatingStars";
+import {useInfiniteQuery} from "@tanstack/react-query";
 import {TextLoader} from "@comps/Loader";
 import Button from "@comps/Button";
+import SearchDoctorItem from "@comps/SearchDoctorItem";
 
 export default function SearchDoctor() {
-    const {data, isLoading, fetchNextPage} = useInfiniteQuery(['search'], ({pageParam = 1}) => {
+    const {data, isLoading, fetchNextPage, hasNextPage} = useInfiniteQuery(['search'], ({pageParam = 1}) => {
         return fetch(`/api/search?term=endoc&offset=${pageParam}`, {
             method: 'GET',
             headers: {
@@ -21,6 +21,21 @@ export default function SearchDoctor() {
         }
     });
 
+    useEffect(() => {
+        let fetching = false;
+        const handleScroll = async () => {
+            const {screenTop, scrollY, innerHeight} = window;
+            if(!fetching && screenTop - scrollY <= innerHeight * 1.2) {
+                fetching = true
+                if(hasNextPage) await fetchNextPage()
+                fetching = false
+            }
+        }
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    }, [fetchNextPage, hasNextPage])
 
 
     if (isLoading) return (<TextLoader />);
