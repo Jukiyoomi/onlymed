d0fc8a6342e5696ce11433f8ff66eb90540e2794
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-07-27 18:53:42 +0200

    reset the search state on app mount #16
    (it solves the page reload persisted state bug)

--- assets/app/index.tsx
+++ assets/app/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Routes, Route} from "react-router-dom";
 import Dashboard from "@pages/Dashboard";
 import Layout from "@comps/Layout";
@@ -6,9 +6,11 @@ import {useQuery} from "@tanstack/react-query";
 import Loader from "@comps/Loader";
 import useUserStore from "./store/user";
 import SearchDoctor from "@pages/SearchDoctor";
+import useSearchStore from "./store/search";
 
 export default function App() {
 	const setUser = useUserStore((state) => state.setUser);
+	const reset = useSearchStore((state) => state.reset);
 	const { isLoading} = useQuery(['user'], async () => {
 		return fetch('/api/dashboard')
 			.then(res => res.json())
@@ -17,6 +19,10 @@ export default function App() {
 				return data;
 			})
 	});
+
+	useEffect(() => {
+		reset();
+	}, [])
 
 	return (
 		<Layout>
