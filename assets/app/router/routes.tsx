23c0fb6934ad933cb89290b3cb7c46f453c46785
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-08-26 13:19:59 +0200

    separate file for routes & new path alias #23

--- assets/app/router/routes.tsx
+++ assets/app/router/routes.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom";
 import Dashboard from "@pages/Dashboard";
 import SearchDoctor from "@pages/SearchDoctor";
 import Params from "@pages/Params";
+import React from "react";
 
-const routes = [
+export default [
 	{
 		path: "/dashboard",
 		component: <Dashboard />
@@ -21,16 +20,4 @@ const routes = [
 		path: "*",
 		component: <h1>Not Found</h1>
 	}
-]
-
-export default function Router() {
-	return (
-		<Routes>
-			{
-				routes.map(({path, component}, id) => (
-					<Route path={path} element={component} key={id} />
-				))
-			}
-		</Routes>
-	)
-}
+];
