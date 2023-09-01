de1ba8ec9d38861f26f329232597c3b2295172fc
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-01 13:03:24 +0200

    add path alias to webpack for build #23

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ Encore
         "@img": path.resolve(__dirname, "assets/images"),
         "@store": path.resolve(__dirname, "assets/app/store"),
         "@hooks": path.resolve(__dirname, "assets/app/hooks"),
+        "@router": path.resolve(__dirname, "assets/app/router"),
     })
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
