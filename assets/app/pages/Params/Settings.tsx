d88cebdbdea085bb908379bf4901516acab7ba99
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-04 18:43:06 +0200

    update mail setting #23
    - install react hook form to handle form requests
    - install zod for schema validation
    - implement mail update setting component

--- assets/app/pages/Params/Settings.tsx
+++ assets/app/pages/Params/Settings.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Button from "@comps/Button";
-import wretch from "wretch";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {ErrorMessage} from "@hookform/error-message";
 
 export default function Settings({currentLink}: {currentLink: string}) {
 	const Element = balancer(currentLink);
@@ -53,11 +56,69 @@ function Account() {
 	}
 }
 
+type MailInputsType = {
+	oldMail: string,
+	newMail: string
+}
+
+const mailSettingsSchema = z.object({
+	oldMail: z.string({
+		required_error: "L'ancienne adresse e-mail est requise."
+	}).email({
+		message: "L'ancienne adresse e-mail doit être valide."
+	}),
+	newMail: z.string({
+		required_error: "La nouvelle adresse e-mail est requise."
+	}).email({
+
+		message: "La nouvelle adresse e-mail doit être valide."
+	}),
+}).refine(data => data.oldMail !== data.newMail, {
+	message: "Les deux adresses e-mail doivent être différentes.",
+	path: ["newMail"]
+})
+
 function Mail() {
+	const { register, handleSubmit, formState: { errors } } = useForm<MailInputsType>({
+		resolver: zodResolver(mailSettingsSchema)
+	});
+	const onSubmit: SubmitHandler<MailInputsType> = data => console.log(data);
+
 	return {
 		Title: "Adresse E-mail",
 		Content: (
-			<></>
+			<div className="settings_content settings_mail">
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="form_half">
+						<div className="form_widget">
+							<label htmlFor="oldMail">Ancienne Adresse E-mail</label>
+							<input
+								type="email"
+								{...register('oldMail', {required: true})}
+							/>
+						</div>
+						<div className="form_widget">
+							<label htmlFor="newMail">Nouvelle Adresse E-mail</label>
+							<input
+								type="email"
+								{...register('newMail', {required: true})}
+							/>
+						</div>
+					</div>
+
+					<Button type="primary">Enregistrer</Button>
+					<ErrorMessage
+						errors={errors}
+						name="newMail"
+						render={({ message }) => <div className="form-error">Error: {message}</div>}
+					/>
+					<ErrorMessage
+						errors={errors}
+						name="oldMail"
+						render={({ message }) => <div className="form-error">Error: {message}</div>}
+					/>
+				</form>
+			</div>
 		)
 	}
 }
