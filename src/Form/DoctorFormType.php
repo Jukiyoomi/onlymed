a1db499e0e9bab0a0819eef1952e52f70984f417
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-05 11:31:01 +0200

    update password length in form #23

--- src/Form/DoctorFormType.php
+++ src/Form/DoctorFormType.php
@@ -91,7 +91,7 @@ class DoctorFormType extends AbstractType
 						'message' => 'Please enter a password',
 					]),
 					new Length([
-						'min' => 6,
+						'min' => 8,
 						'minMessage' => 'Your password should be at least {{ limit }} characters',
 						// max length allowed by Symfony for security reasons
 						'max' => 4096,
