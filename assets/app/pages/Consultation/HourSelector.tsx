6d796c53fa74a940bff351ec1372b49953183c1d
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-20 17:59:23 +0200

    component to pick appointment #27
    - custom component to pick date
    - component to pick time as well (some are disabled if already registered as appt)

---
+++ assets/app/pages/Consultation/HourSelector.tsx
@@ -0,0 +1,115 @@
+import React, {useEffect, useState} from "react";
+import DatePicker from "react-date-picker";
+import {CalendarCheck, X} from "lucide-react";
+import Button from "@comps/Button";
+import wretch from "wretch";
+import {useMutation} from "@tanstack/react-query";
+
+type Props = {
+    disabledDates: number[],
+    doctorId: number
+}
+
+export default function HourSelector({disabledDates, doctorId}: Props) {
+    const [date, setDate] = React.useState<Date>(new Date());
+    const [selectedHour, setSelectedHour] = useState<number | null>(null);
+    const selectedDateRef =
+        !!selectedHour ? (new Date(selectedHour * 1000)).toISOString().slice(0, 19).replace('T', ' ') :
+        null
+    ;
+
+    const onDateSelect = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        setSelectedHour(Number(e.currentTarget.dataset.timestamp));
+    }
+
+    const {mutateAsync, data, isLoading} = useMutation(() => {
+        return wretch("/api/appointments")
+            .post({
+                date: selectedDateRef,
+                doctorId,
+            })
+            .json(async (res) => res)
+    })
+
+    return (
+        <>
+            <DatePicker
+                value={date}
+                onChange={(date) => setDate(new Date(date as Date))}
+                minDate={new Date()}
+                calendarIcon={<CalendarCheck />}
+                clearIcon={<X />}
+                format="dd/MM/yyyy"
+            />
+
+            <Hours selectedDate={date.toLocaleDateString()} disabledDates={disabledDates} onDateSelect={onDateSelect} />
+
+            {
+                !!selectedDateRef ? <p>Vous avez sélectionné {selectedDateRef}</p> :
+                    <p>Veuillez sélectionner une date et une heure</p>
+            }
+            <Button
+                type="primary"
+                onClick={() => mutateAsync()}
+                disabled={isLoading || !selectedDateRef}
+            >
+                {isLoading ? "Chargement..." : "Valider"}
+            </Button>
+        </>
+    )
+}
+
+type HoursProps = {
+    selectedDate: string,
+    disabledDates: number[],
+    onDateSelect: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
+}
+
+type DisplayHour = {
+    date: number;
+    label: string;
+}
+
+const Hours = ({selectedDate, disabledDates, onDateSelect}: HoursProps) => {
+    const [hours, setHours] = useState<DisplayHour[]>([]);
+    const formattedDateRef = selectedDate.split("/").reverse().join("-"); // Format date using hyphens
+
+    // Generate hours from 8AM to 5PM
+    const generateHours = () => {
+        const newHours: DisplayHour[] = [];
+        for (let hour = 8; hour <= 17; hour++) {
+            const hourToString = (hour < 10) ? "0" + hour : String(hour);
+            const timestamp = Date.parse(formattedDateRef + "T" + hourToString + ":00:00");
+            const date = new Date(timestamp);
+            newHours.push({
+                date: timestamp / 1000,
+                label: date.getHours() + "h",
+            });
+
+        }
+        setHours(newHours);
+    };
+
+    useEffect(() => {
+        generateHours();
+    }, [selectedDate]);
+
+    return (
+        <ul>
+            {hours.map((hour, index) => (
+                <li
+                    key={index}
+                    onClick={onDateSelect}
+                    data-timestamp={hour.date}
+                    style={{
+                        backgroundColor: disabledDates.includes(hour.date) ? "red" : "blue",
+                        pointerEvents: disabledDates.includes(hour.date) ? "none" : "auto",
+                        opacity: disabledDates.includes(hour.date) ? 0.6 : 1,
+                    }}
+                >
+                    {hour.label} - {disabledDates.includes(hour.date) ? "Indisponible" : "Disponible"}
+                </li>
+            ))}
+        </ul>
+    );
+}
--- assets/app/pages/Consultation/index.tsx
+++ assets/app/pages/Consultation/index.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {useParams} from "react-router-dom";
 import useLoader from "./loader";
 import Container from "@comps/Container";
+import RatingStars from "@comps/RatingStars";
+import HourSelector from "./HourSelector";
 
 export default function Consultation() {
     const {id} = useParams();
 
-	const {data: doctor, isLoading} = useLoader(id!);
+	const {data, isLoading, error} = useLoader(id!);
+
+	const dates = useRef([
+		1695290400,
+		1697284800,
+	]).current;
+
 	if (isLoading) return <div>Loading...</div>
 
+	if (error) {
+		console.log(error);
+		return <div>{JSON.stringify(error)}</div>
+	}
+
     return (
-		<Container>
-			<h1>Consultation of {id}</h1>
-			<div>
-				<pre>{JSON.stringify(doctor, null, 2)}</pre>
-			</div>
+		<Container className="doctor_detail">
+			<header>
+				<img
+					src="https://www.tomsguide.fr/content/uploads/sites/2/2020/12/tony-stark-robert.jpg"
+					alt={`Image of ${data?.doctor.firstname} ${data?.doctor.lastname}`}
+					width={200}
+				/>
+				<p>Dr. {data?.doctor.firstname} {data?.doctor.lastname}</p>
+				<RatingStars rating={Math.round(Math.random() * 5)} />
+			</header>
+
+			<HourSelector disabledDates={dates} doctorId={Number(id!)} />
 		</Container>
 	);
 }
