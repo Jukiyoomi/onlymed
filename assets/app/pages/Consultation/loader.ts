fa7e788da179c52192bd3b77c5ab739ff5043f1f
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-25 19:52:00 +0200

    change from then to await api

--- assets/app/pages/Consultation/loader.ts
+++ assets/app/pages/Consultation/loader.ts
@@ -1,27 +1,20 @@
 import {useQuery} from "@tanstack/react-query";
 import doctorSchema, {Doctor} from "@schemas/doctor";
 import wretch from "wretch";
-import {ZodError} from "zod";
 
 export default function useLoader(id: string) {
     return useQuery<Doctor>({
         queryKey: ['doctor', id],
         queryFn: async () => {
-            return wretch()
+            const data = await wretch()
                 .get(`/api/doctors/${id}`)
-                .json(async (res) => doctorSchema.parse(res))
-                .then((res) => {
-                    throw new Error("Une erreur est survenue. Veuillez recharger la page hihi.");
-                })
-                .catch((e: ZodError) => {
-                    console.log(e);
-                    return e;
-                })
-                .catch((e) => {
-                    const parsedError = JSON.parse(e.message);
-                    console.log(parsedError.error);
-                    return parsedError;
-                })
+                .json(async (res) => res);
+
+            if (data.error) {
+                throw new Error(JSON.stringify(data));
+            }
+
+            return doctorSchema.parse(data);
         }
     });
 }
