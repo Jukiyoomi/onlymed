0ebff85fbcd8f0b0ad8af208aece7e946c13cc4b
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-24 11:41:07 +0200

    display doctor address on dashboard

--- assets/app/pages/Dashboard/doctor/index.tsx
+++ assets/app/pages/Dashboard/doctor/index.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import {toString} from "@/store/user";
+import useUserStore, {toString} from "@/store/user";
 import {Link} from "react-router-dom";
 import Button from "@/components/Button";
 import ApptsSection from "./ApptsSection";
 import Container from "@/components/Container";
 
 export default function DoctorDashboard() {
+	const user = useUserStore((state) => state.user);
 	console.log("DoctorDashboard")
 	return (
 		<Container className="dashboard">
 			<section className="dashboard_header">
-				<h1 className="main-title">Dr {toString()}</h1>
+				<h1 className="main-title">Dr {toString()} caca</h1>
 				<Link to="/dashboard/params">
 					<Button type="secondary">
 						Voir les options
 					</Button>
 				</Link>
 			</section>
+
+			<p>{user?.address
+					? `Mon adresse : ${user.address}`
+					: "Veuillez renseigner votre adresse pour que les patients puissent vous trouver"}
+			</p>
 
 			<section className="dashboard_content">
 				<ApptsSection />
